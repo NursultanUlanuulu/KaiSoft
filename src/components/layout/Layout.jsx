b90cc4d353ab08071ad893ebd2084ec6f33d2a79
unknown <ulanuulunursultan36@gmail.com>
2024-01-24 11:06:58 +0600

    finish company

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,4 +1,5 @@
 import About from "../about/About";
+import Company from "../company/Company";
 import Header from "./Header";
 
 export default function Layout() {
@@ -6,6 +7,7 @@ export default function Layout() {
     <div>
       <Header />
       <About />
+      <Company />
     </div>
   );
 }
