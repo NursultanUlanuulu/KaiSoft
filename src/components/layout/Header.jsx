75c1ef69da3d302d96d0ee39faeaa4f941efb548
unknown <ulanuulunursultan36@gmail.com>
2024-03-09 11:45:49 +0600

    update header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,17 +3,18 @@ import logo from "../../assets/images/header/logo.png";
 import bg from "../../assets/images/header/bg.png";
 import styled from "styled-components";
 import Button from "../UI/Button";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Header() {
   return (
     <Rectangle>
-      <div className=" border-b border-[#A1A1A1]  h-[104.02px]">
+      <div className=" border-b border-[#A1A1A1] h-[90px] lg:h-[123px]">
         <Container>
           <div className="flex items-center justify-between">
-            <div className="w-[204px] py-[22px]">
-              <img src={logo} width={200} />
+            <div className="w-[150px] lg:w-[200px]">
+              <img src={logo} alt="Logo" />
             </div>
-            <div>
+            <div className="hidden md:flex">
               <nav>
                 <ul className="flex justify-between w-[650px]">
                   <Li>Главная</Li>
@@ -24,17 +25,21 @@ export default function Header() {
                 </ul>
               </nav>
             </div>
+            <button className=" inline-block md:hidden text-white">
+              <MenuIcon />
+            </button>
           </div>
-          <div className="flex flex-col justify-center items-center h-[600px]">
-            <p className="text-white text-center text-base font-medium mb-3">
+          <div className="flex flex-col pt-5 lg:justify-center items-center h-[600px]">
+            <p className="text-white text-center text-base lg:font-medium font-light">
               Передовая IT студия
             </p>
-            <p className="text-white text-center text-5xl font-extrabold mb-9">
-              Мы создаем легкие решения <br /> сложных задач и проблем
+            <p className="text-white text-center text-2xl lg:text-5xl  font-extrabold mb-9">
+              Мы создаем легкие решения сложных задач и проблем
             </p>
-            <Button className="bg-[#4985FF] rounded-[30px] text-white w-[200px] h-[45px]">
+            <Button className="hidden sm:inline-block md:inline-block   bg-[#4985FF] text-white  lg:hidden  rounded-[30px] lg:w-[200px] lg:h-[45px]">
               Наши проекты
             </Button>
+            <a className="hiden text-white underline">Наши проекты</a>
           </div>
         </Container>
       </div>
@@ -58,5 +63,14 @@ const Rectangle = styled.header`
   background-image: url(${bg});
   background-size: cover;
   background-position: center;
-  height: 800px;
+
+  @media (min-width: 320px) {
+    height: 400px;
+  }
+  @media (min-width: 768px) {
+    height: 600px;
+  }
+  @media (min-width: 1024px) {
+    height: 800px;
+  }
 `;
