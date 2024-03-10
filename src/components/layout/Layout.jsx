41ce8e495ba1ae1104260fa8fc1f79b9efc11240
unknown <ulanuulunursultan36@gmail.com>
2024-03-10 17:23:42 +0600

    fixed about second block

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,20 +1,11 @@
+import Home from "../../pages/Home";
 import Header from "../Header/Header";
-import About from "../about/About";
-import Company from "../company/Company";
-import AllServises from "../allServices/AllServises";
-import Header from "./Header";
-import AllProjects from "../allProjects/AllProjects";
-import Subcribe from "../subcribe/Subcribe";
 
 export default function Layout() {
   return (
     <div>
       <Header />
-      <About />
-      <Company />
-      <AllServises />
-      <AllProjects/>
-      <Subcribe/>
+      <Home />
     </div>
   );
 }
