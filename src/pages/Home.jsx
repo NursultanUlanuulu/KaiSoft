dfc24b480e4515aca499d1f22106e4ab5180ad1a
unknown <ulanuulunursultan36@gmail.com>
2024-03-25 11:46:44 +0600

    сделал процесс стажировки

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,14 @@ import AllServises from "../components/allServices/AllServises";
 import Subcribe from "../components/subcribe/Subcribe";
 import AllProjects from "../components/allProjects/AllProjects";
 import HeroSection from "../components/Header/HeroSection";
+import InternProgress from "../components/internProgress/InternProgress";
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <About />
+      <InternProgress />
       <AllServises />
       <AllProjects />
       <Subcribe />
