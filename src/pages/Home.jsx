9c94ee26f4d30271e74e5cfef5c9084cf1fca356
unknown <ulanuulunursultan36@gmail.com>
2024-03-25 12:00:43 +0600

    удалил компонент

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import About from "../components/about/About";
-import AllServises from "../components/allServices/AllServises";
 import Subcribe from "../components/subcribe/Subcribe";
 import AllProjects from "../components/allProjects/AllProjects";
 import HeroSection from "../components/Header/HeroSection";
@@ -11,7 +10,6 @@ const Home = () => {
       <HeroSection />
       <About />
       <InternProgress />
-      <AllServises />
       <AllProjects />
       <Subcribe />
     </div>
